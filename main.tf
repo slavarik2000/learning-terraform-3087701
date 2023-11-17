74942c3a8fbed388d6dd76117d7dafd37f32782d
slavarik2000 <39342629+slavarik2000@users.noreply.github.com>
2023-11-17 17:42:58 -0500

    aws_security_group

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.0"
+  version = "4.13.0"
   name    = "blog_new"
 
   vpc_id = data.aws_vpc.default.id
