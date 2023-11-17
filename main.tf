64944d6ab5fff4693cefe4963a8f91a458f1563d
slavarik2000 <39342629+slavarik2000@users.noreply.github.com>
2023-11-17 16:02:08 -0500

    fix2

--- main.tf
+++ main.tf
@@ -29,14 +29,6 @@ resource "aws_instance" "blog" {
 }
 
 resource "aws_security_group" "blog" {
-  name = "blog"
-  tags = {
-    Terraform = "true"
-  }
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow http and https in. Allow everything out"
 
