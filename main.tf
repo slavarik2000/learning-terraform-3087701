57c12d1d20986518f78113a1eb00bbecfb1c3067
slavarik2000 <39342629+slavarik2000@users.noreply.github.com>
2023-11-18 14:22:50 -0500

    aws_lb_listener

--- main.tf
+++ main.tf
@@ -73,6 +73,23 @@ module "alb" {
       cidr_ipv4   = "0.0.0.0/0"
     }
   }
+
+  resource "aws_lb_listener" "front_end" {
+    load_balancer_arn = aws_lb.front_end.arn
+    port              = "80"
+    protocol          = "HTTP"
+
+    default_action {
+      type = "fixed-response"
+
+      fixed_response {
+        content_type = "text/plain"
+        message_body = "Fixed response content"
+        status_code  = "200"
+      }
+    }
+  }
+
   security_group_egress_rules = {
     all = {
       ip_protocol = "-1"
