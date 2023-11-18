25bd22954aa3155f1a4c0e3d5f75980da49af139
slavarik2000 <39342629+slavarik2000@users.noreply.github.com>
2023-11-18 14:42:25 -0500

    load b

--- main.tf
+++ main.tf
@@ -107,6 +107,12 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
     }
   }
 
