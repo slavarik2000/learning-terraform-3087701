ca5eb4bc780189e1708da0c41651e94279b6acd8
slavarik2000 <39342629+slavarik2000@users.noreply.github.com>
2023-11-18 14:25:00 -0500

    f

--- main.tf
+++ main.tf
@@ -48,22 +48,6 @@ resource "aws_instance" "blog" {
   }
 }
 
-resource "aws_lb_listener" "front_end" {
-  load_balancer_arn = aws_lb.front_end.arn
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type = "fixed-response"
-
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Fixed response content"
-      status_code  = "200"
-    }
-  }
-}
-
 module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
