0e5e1c8f31a6cc3d189ce156a27c46d1d5684ff1
slavarik2000 <39342629+slavarik2000@users.noreply.github.com>
2023-11-19 11:59:30 -0500

    fix

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "blog_vpc" {
   cidr = "${var.environment.network_prefix}.0.0/16"
 
   azs             = ["ca-central-1a", "ca-central-1b", "ca-central-1d"]
-  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix.102.0/24", "${var.environment.network_prefix.103.0/24"]
+  public_subnets  = ["${var.environment.network_prefix}.101.0/24", "${var.environment.network_prefix}.102.0/24", "${var.environment.network_prefix}.103.0/24"]
 
 
   tags = {
